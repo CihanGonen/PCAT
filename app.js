991ae7e83dbd58e4539e6ef746d25fa23541bfae
Cihan <cihangonen35@gmail.com>
2021-05-28 17:23:34 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 const port = 3000;
 app.listen(port, () => {
