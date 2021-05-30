6153a9084e032de0497675b079e7e8bdac9c5029
Cihan <cihangonen35@gmail.com>
2021-05-30 22:25:56 +0300

    photo add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
-const ejs = require('ejs');
+const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 const Photo = require('./models/Photo');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost/pcat-test-db', {
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
+app.use(fileUpload());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -43,6 +45,23 @@ app.get('/add', (req, res) => {
 
 app.post('/photos', async (req, res) => {
   await Photo.create(req.body);
+  res.redirect('/');
+});
+
+const uploadDir = 'public/uploads';
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+let uploadeImage = req.files.image;
+let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+uploadeImage.mv(uploadPath, async () => {
+  await Photo.create({
+    ...req.body,
+    image: '/uploads/' + uploadeImage.name,
+  });
   res.redirect('/');
 });
 
